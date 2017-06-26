5f5a34dd066185d97f2aaa041655b3bf02390f6a
SONG Jianzhong <metaseed@gmail.com>
2017-06-26 11:28:14 +0800

    yes

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
+
+// Disable Angular’s development mode, which turns off assertions and other checks within the framework
+//enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
